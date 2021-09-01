3f5bdf01841deaf43b588ccbff98de1532bef2fc
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-09-01 21:10:22 +0530

    ADDED: Maximum of 3 Strings.

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,21 +1,21 @@
 package com.bl.generics;
 
 public class MaxFinder {
-    // returns the max value of three Floats
-    static Float findMax(Float float1, Float float2, Float float3) {
-        Float max = float1;
-        if(float2.compareTo(max) > 0)
-            max = float2;
-        if (float3.compareTo(max) > 0)
-            max = float3;
-        System.out.printf("Max value of %s %s %s is %s. \n", float1, float2, float3, max);
+    // returns the max value of three strings
+    static String findMax(String string1, String string2, String string3) {
+        String max = string1;
+        if(string2.compareTo(max) > 0)
+            max = string2;
+        if (string3.compareTo(max) > 0)
+            max = string3;
+        System.out.printf("Max value of %s %s %s is %s. \n", string1, string2, string3, max);
 
         return max;
     }
 
     public static void main(String[] args) {
-        Float float1 = 51.5f, float2 = 10.1f, float3 = 5.01f;
-        Float max = findMax(float1, float2, float3);
+        String string1 = "Apple", string2 = "Peach", string3 = "Banana";
+        String max = findMax(string1, string2, string3);
     }
 
 }
